d288820ef615002329d0f8a92696a4cf0a21234b
Dave Moten <davidmoten@gmail.com>
2014-04-01 17:45:56 +1100

    return something from email servlet

--- src/main/java/com/github/davidmoten/email/EmailServlet.java
+++ src/main/java/com/github/davidmoten/email/EmailServlet.java
@@ -51,6 +51,17 @@ public class EmailServlet extends HttpServlet {
 		sendgrid.setText(s.toString());
 
 		sendgrid.send();
+
+		// Set to expire far in the past.
+		resp.setHeader("Expires", "-1");
+		// Set standard HTTP/1.1 no-cache headers.
+		resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+		// Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+		resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
+		// Set standard HTTP/1.0 no-cache header.
+		resp.setHeader("Pragma", "no-cache");
+
+		resp.getWriter().write("sid=somebody&status=1");
 	}
 
 }
