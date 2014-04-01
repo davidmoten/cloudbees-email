00d87138d7884ee0c21b7ea57441b1b5626b8f07
Dave Moten <davidmoten@gmail.com>
2014-04-01 16:53:40 +1100

    initial commit

---
+++ src/main/java/com/github/davidmoten/email/EmailServlet.java
@@ -0,0 +1,52 @@
+package com.github.davidmoten.email;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.sendgrid.SendGrid;
+
+public class EmailServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4925620975592532511L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String username = System.getProperty("SENDGRID_USERNAME");
+		String password = System.getProperty("SENDGRID_PASSWORD");
+
+		StringBuilder s = new StringBuilder();
+		@SuppressWarnings("unchecked")
+		Enumeration<String> en = req.getAttributeNames();
+		while (en.hasMoreElements()) {
+			String key = en.nextElement();
+			s.append(key);
+			s.append("=");
+			s.append(req.getParameter(key));
+			s.append("\n");
+		}
+		s.append("sent via SendGrid");
+
+		SendGrid sendgrid = new SendGrid(username, password);
+
+		sendgrid.addTo("davidmoten@gmail.com");
+		sendgrid.setFrom("davidmoten@gmail.com");
+		sendgrid.setSubject("hello from cloudbees-email app");
+		sendgrid.setText(s.toString());
+
+		sendgrid.send();
+	}
+
+}
