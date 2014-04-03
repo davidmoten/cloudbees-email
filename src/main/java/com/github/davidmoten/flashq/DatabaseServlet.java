7d24db2c773e6c6ac1cbffa04e3d03de738923b4
Dave Moten <davidmoten@gmail.com>
2014-04-03 16:16:55 +1100

    add logging

--- src/main/java/com/github/davidmoten/flashq/DatabaseServlet.java
+++ src/main/java/com/github/davidmoten/flashq/DatabaseServlet.java
@@ -15,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import rx.functions.Action1;
 
 import com.github.davidmoten.rx.jdbc.ConnectionProvider;
@@ -23,6 +26,9 @@ import com.github.davidmoten.rx.jdbc.Database;
 public class DatabaseServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4137504003076597022L;
+
+	private static final Logger log = LoggerFactory
+			.getLogger(DatabaseServlet.class);
 	private Database db;
 
 	@Override
@@ -39,6 +45,7 @@ public class DatabaseServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		log.info("params=" + Parameters.toString(req));
 		Util.setNoCache(resp);
 		if ("true".equals(req.getParameter("list")))
 			list(resp);
--- src/main/java/com/github/davidmoten/flashq/LoginServlet.java
+++ src/main/java/com/github/davidmoten/flashq/LoginServlet.java
@@ -8,13 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoginServlet extends HttpServlet {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(LoginServlet.class);
 
 	private static final long serialVersionUID = 3525431883928567774L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String params = Parameters.toString(req);
+		log.info(params);
 		if (req.getParameter("uid") == null)
 			throw new ServletException("uid parameter not specified");
 		String userId = req.getParameter("uid").trim();
